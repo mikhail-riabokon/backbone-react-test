8c1f3aa0b421f9c7d7bf1142471c19b6192dd6dd
mikhail-riabokon <mikhail.riabokon@gmail.com>
2017-03-08 19:42:52 +0200

    helpers

---
+++ src/helpers/withCollection.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function withCollection(WrappedComponent, collection) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        models: [],
+      };
+
+      collection.on('add', this.onModelsAdded, this);
+    }
+
+    onModelsAdded() {
+      this.setState({ models: collection.models });
+    }
+
+    componentDidMount() {
+      collection.fetch();
+    }
+
+    componentWillUnmount() {
+      collection.off('add', this.onModelsAdded, this);
+    }
+
+    render() {
+      return (
+        <WrappedComponent
+          { ...this.props }
+          collection={collection}
+          models={this.state.models}
+        />
+      );
+    }
+  }
+}
